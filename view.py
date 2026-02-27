8cbb7425bad13e18067c154f9b43fe366ec4e541
Pun viraksakada <ahmok4081@gmail.com>
2026-02-27 22:20:01 +0700

    hello viewer

--- view.py
+++ view.py
@@ -1,4 +1,6 @@
-a = True
-
-if a == True:
-    print("NO")
+print("Welcome to the the word guessing game ")
+user_input = input("please selclter yes or no to play y/n ").lower
+if user_input == "n":
+    print("welcome to the game")
+else :
+    print('please try again ')
