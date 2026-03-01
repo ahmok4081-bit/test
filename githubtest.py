dd46e5c0ed3e724f828e27cea45e4bf52d494437
Pun viraksakada <ahmok4081@gmail.com>
2026-03-01 23:00:11 +0700

    test10

---
+++ githubtest.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+# read a line from the user, then convert it to int
+a = int(input("please enter the number "))
+
+if a > 0:
+    print("hello vatey")
+else:
+    print("dumb sakada")
